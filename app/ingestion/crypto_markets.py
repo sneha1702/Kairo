6684561d46e27053747412aa09a96ae231dd4134
Sneha <snehaparihar17@gmail.com>
2026-06-11 14:33:16 +0200

    pre-edit snapshot 2026-06-11 14:33:16

--- app/ingestion/crypto_markets.py
+++ app/ingestion/crypto_markets.py
@@ -22,6 +22,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from datetime import datetime, timezone
 from typing import Optional
 
+import certifi
 import pymongo
 import requests
 from bs4 import BeautifulSoup
