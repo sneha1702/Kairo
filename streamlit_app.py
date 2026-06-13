4ec57b18f28b4200ed08128099cd9f291f207690
Sneha <snehaparihar17@gmail.com>
2026-06-13 13:35:28 +0200

    pre-edit snapshot 2026-06-13 13:35:28

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ ROOT_PATH = Path(__file__).resolve().parent
 if str(ROOT_PATH) not in sys.path:
     sys.path.insert(0, str(ROOT_PATH))
 
-import app.app
-app.app.run()
+from app.app import run as _run
+_run()
